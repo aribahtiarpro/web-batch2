e9e73524ed04dd5bedb91e2b551ae0b131f934fd
aribahtiarpro <aribahtiarpro@gmail.com>
2019-04-08 19:58:39 +0700

    tambah file web portofolio

--- aribahtiar/blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ aribahtiar/blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->string('avatar');
+            $table->string('provider');
+            $table->string('provider_id');
+
             $table->rememberToken();
             $table->timestamps();
         });
